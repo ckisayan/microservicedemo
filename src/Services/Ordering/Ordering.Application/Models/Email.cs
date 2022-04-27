dde50f8d33942f58a390bfee17adf85a8068ff5a
gitName <a@b.c>
2022-04-27 16:32:12 -0700

    Ordering.Application completed

---
+++ src/Services/Ordering/Ordering.Application/Models/Email.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Models
+{
+    //Basic email DTO object
+    public class Email
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
