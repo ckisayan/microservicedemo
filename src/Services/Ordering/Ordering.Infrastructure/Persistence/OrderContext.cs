3ca702e21857a7ce811be53d1426b1a9cb1f0fed
gitName <a@b.c>
2022-05-04 19:52:50 -0700

    order API wired up with SQL server

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext : DbContext  //install-package Microsoft.EntityFrameworkCore.SqlServer 5.* not latest
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "chris";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "chris";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
